e0957b891cdf22f7d46b4f803483f8524f8aad14
SirawitN <nine_sirawit@hotmail.com>
2022-04-29 22:05:09 +0700

    update the Company schema

--- model/Company.js
+++ model/Company.js
@@ -1,48 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const CompanySchema = new mongoose.Schema({
+const CompanySchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Please add a name'],
-        unique: true,
-        trim: true,
-        maxlength: [50, 'Name cannot be more than 50 characters'],
+      type: String,
+      required: [true, "Please add a name"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Name cannot be more than 50 characters"],
     },
     address: {
-        type: String,
-        required: [true, 'Please add an address'],
+      type: String,
+      required: [true, "Please add an address"],
     },
     website: {
-        type: String,
-        require: [true, 'Please add a website'],
+      type: String,
+      require: [true, "Please add a website"],
     },
     description: {
-        type: String,
-        required: [true, 'Please add a description'],
+      type: String,
+      required: [true, "Please add a description"],
     },
-    tel: {
-        type: String,
-    },    
-},
-    {
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true },
-    }
+    telephoneNumber: {
+      type: String,
+      required: [true, "Please add a telephone number"],
+      match: [
+        /[0-9]{3}-[0-9]{3}-[0-9]{4}$/,
+        "Please add a valid telephone number (format: 000-0000000)",
+      ],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
 
 //* Reverse populate with virtuals
-CompanySchema.virtual('InterviewSession', {
-    ref: 'InterviewSession',
-    localField: '_id',
-    foreignField: 'company',
-    justOne: false,
+CompanySchema.virtual("InterviewSession", {
+  ref: "InterviewSession",
+  localField: "_id",
+  foreignField: "company",
+  justOne: false,
 });
 
 //* Cascade delete InterviewSession when a hospital is deleted
-CompanySchema.pre('remove', async function (next) {
-    console.log(`InterviewSession being removed from company ${this._id}`);
-    await this.model('InterviewSession').deleteMany({ company: this._id });
-    next();
+CompanySchema.pre("remove", async function (next) {
+  console.log(`InterviewSession being removed from company ${this._id}`);
+  await this.model("InterviewSession").deleteMany({ company: this._id });
+  next();
 });
 
-module.exports = mongoose.model('company', CompanySchema);
+module.exports = mongoose.model("company", CompanySchema);
